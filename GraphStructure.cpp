87d6f19e7cf7a84ba6c335ad99c9998f2d20a0a2
Sandip Pal <sandippal@yahoo.com>
2018-08-12 11:21:17 -0700

    Initial Graph Algorithm files

---
+++ GraphStructure.cpp
@@ -0,0 +1,9 @@
+//
+//  GraphStructure.cpp
+//  GraphAlgo
+//
+//  Created by Sandip Pal on 8/12/18.
+//  Copyright © 2018 Sandip Pal. All rights reserved.
+//
+
+#include "GraphStructure.hpp"
---
+++ GraphStructure.hpp
@@ -0,0 +1,14 @@
+//
+//  GraphStructure.hpp
+//  GraphAlgo
+//
+//  Created by Sandip Pal on 8/12/18.
+//  Copyright © 2018 Sandip Pal. All rights reserved.
+//
+
+#ifndef GraphStructure_hpp
+#define GraphStructure_hpp
+
+#include <stdio.h>
+
+#endif /* GraphStructure_hpp */
